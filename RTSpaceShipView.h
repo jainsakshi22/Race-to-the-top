51fa92a2868cc71dc80bb7a6a44d38ca2e74b96f
jainsakshi22 <jainsakshi22@gmail.com>
2014-11-04 00:54:29 +0530

    Created project

---
+++ RTSpaceShipView.h
@@ -0,0 +1,13 @@
+//
+//  RTSpaceShipView.h
+//  Race to the top
+//
+//  Created by Sakshi Jain on 04/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTSpaceShipView : UIView
+
+@end
---
+++ RTSpaceShipView.m
@@ -0,0 +1,21 @@
+//
+//  RTSpaceShipView.m
+//  Race to the top
+//
+//  Created by Sakshi Jain on 04/11/14.
+//
+//
+
+#import "RTSpaceShipView.h"
+
+@implementation RTSpaceShipView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+@end
