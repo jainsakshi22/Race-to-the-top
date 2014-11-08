257c726fc45f7aa58fa2c90a88203cab2950487a
jainsakshi22 <jainsakshi22@gmail.com>
2014-11-08 14:51:58 +0530

    How storyboard files are saved

--- Race to the top/RTPathView.m
+++ Race to the top/RTPathView.m
@@ -11,6 +11,32 @@
 
 @implementation RTPathView
 
+//Purpose of this method here? as we wil never call this method from story board, we are actuall archiving it from storyboard
+//-(id)initWithFrame:(CGRect)frame
+//{
+//    self = [super initWithFrame:frame];
+//    if (self)
+//    {
+//        [self setup];
+//    }
+//    return self;
+//}
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        [self setup];
+    }
+    return self;
+}
+
+-(void)setup
+{
+    self.backgroundColor = [UIColor clearColor];
+}
+
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
